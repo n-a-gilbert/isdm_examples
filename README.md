75659aee299b46755a5812693f59b9c6dd836499
Neil Gilbert <n.a.gilbert92@gmail.com>
2020-06-24 12:17:28 -0500

    commented code and added a few things

--- README.md
+++ README.md
@@ -2,5 +2,49 @@
 
 ### NOTE: Using NIMBLE
 
-These models are written in NIMBLE, a language derived from BUGS. Unlike previous programs (WinBUGS, JAGS), NIMBLE models are programmable objects in R, but are compiled in C++ for speed. Therefore, prior to installing NIMBLE, you must have Rtools installed so the code can be compiled. Please see the [NIMBLE website](https://r-nimble.org/download) for instructions
+These models are written in NIMBLE, a language derived from BUGS. Unlike previous programs (WinBUGS, JAGS), NIMBLE models are programmable objects in R, but are compiled in C++ for speed. Therefore, prior to installing NIMBLE, you must have Rtools installed so the code can be compiled. Please see the [NIMBLE website](https://r-nimble.org/download) for instructions.
+
+### Data
+
+The data included in this repository consists of four objects:
+
+1. constants : constants (e.g. indices) for the model. In list form.
+2. data : data for the model. In list form.
+3. cov : two covariates, in a polygon shapefile
+4. test : a dataframe containing testing data to evaluate predictive performance
+
+More detail about each is as follows:
+
+#### constants
+
+* ncell : the number of grid cells (8.5x8.5 km)
+* cell : the identity of grid cells containing a camera
+* nsite : the number of cameras
+* ncounty: the number of counties
+* low: a lower bounding index used to define which grid cells fall within which county
+* high: an upper bounding index used to define which grid cells fall within which county
+* k: the number of adjacencies between grid cells 
+
+#### data
+
+* y : the number of week-long sampling occasions bear was detected at a given camera
+* nsuryveys: the number of week-long sampling occasions the camera was active for
+* e: an effort offset used in the harvest model. This is, for each county, the number of harvest authorizations per square kilometer of the bear management zone in which the county falls
+* w: the number of bears harvested in each county in 2018
+* num: number of adjacent grid cells to each grid cell
+* adj: the identies of adjacent grid cells for each grid cell
+* weights: how neighboring grid cells are weighed. All = 1
+* forest: mean % canopy cover within each 8.5x8.5 km grid cell
+* imperv: mean % impervious cover within each 8.5x8.5 km grid cell
+
+#### cov
+
+Shapefile of covariates (vectorized grid).
+
+#### test
+
+Camera data witheld to evaluate predictive performance of model via AUC. A dataframe with two columns:
+
+* INDEX: the identity of the grid cell that the camera falls inside of
+* Y: Indicator of whether (1) or not (0) bear was ever detected at the camera during 2018
 
--- code/bear_joint.R
+++ code/bear_joint.R
@@ -1,65 +1,65 @@
+# .......................................................................
+# REQUIRED PACKAGES
+# .......................................................................
+# install.packages(c("here", "nimble", "coda", "pROC", "stringr", "sf"))
 library(here)
 library(nimble)
 library(coda)
+library(pROC)
+library(stringr)
+library(sf)
 
 setwd(here::here("data"))
-
 load("isdm_data_constants.RData")
+cov <- st_read("covariates.shp")
 
+# inspect data and constants used in modeling
+# refer to README file for description of what they are
 str(data)
 str(constants)
 
-inits <- function() {
-  base::list(z = rep(1, constants$ncell),
-             alpha = runif(1,-1,1), 
-             b_forest = runif(1, -1, 1),
-             b_imperv = runif(1, -1, 1),
-             tau = rgamma(1, 1, 1), 
-             s = rep(0, base::length(data$num)),
-             p = runif(1, 0, 1),
-             eps_psi = rnorm(constants$ncell, 0, runif(constants$ncell, 0, 1)),
-             sd_psi = runif(1, 0, 2))
-}
+# .......................................................................
+# MODEL CODE
+# .......................................................................
 
 joint <- nimble::nimbleCode( {
   
-  # CAR prior for spatial random effects
-  s[1:ncell] ~ dcar_normal(adj[1:k],
-                           weights[1:k],
-                           num[1:ncell],
-                           tau)
+  # .............................................................
+  # PRIORS
+  # .............................................................
   
+  # CAR prior for spatial random effect
+  s[1:ncell] ~ dcar_normal(adj[1:k], weights[1:k], num[1:ncell], tau)
+  # precision of CAR prior
   tau ~ dgamma(1, 1)
   
-  # Other priors
+  # intercept of SDM
   alpha ~ dlogis(0, 1)
+  # regression coefficient for canopy cover
   b_forest ~ dlogis(1, 1)
+  # regression coefficient for impervious cover
   b_imperv ~ dlogis(-1, 1)
+  # detection probability
   p ~ dunif(0, 1)
   
-  for(i in 1:ncell){
-    eps_psi[i] ~ dnorm(0, sd = sd_psi)
-  }
+  # .............................................................
+  # LIKELIHOOD
+  # .............................................................
   
-  sd_psi ~ dunif(0, 2)
-  
-  # prediction-resolution grid
+  # SDM - model for the latent state
   for(i in 1:ncell){
-    log(lambda[i]) <- min(s[i]
-                          + alpha
-                          + b_forest*forest[i]
-                          + b_imperv*imperv[i]
-                          + eps_psi[i], 10)
+    log(lambda[i]) <- min(s[i] + alpha + b_forest*forest[i] + b_imperv*imperv[i], 10)
     psi[i] <- 1 - exp(-lambda[i])
     z[i] ~ dbern(psi[i])
   }
   
-  # cameras within prediction-resolution grid
+  # Camera submodel
   for(j in 1:nsite){
     muy[j] <- z[cell[j]]*p
     y[j] ~ dbin(muy[j], nsurveys[j])
   }
   
+  # Harvest submodel
   for(i in 1:ncounty){
     # e: effort coefficient
     # low & high: indices to account for number of cells within each county
@@ -69,6 +69,20 @@ joint <- nimble::nimbleCode( {
   
 } )
 
+# .......................................................................
+# PREPARE MODEL TO RUN
+# .......................................................................
+
+# function to provide random initial values for parameters
+inits <- function() {
+  base::list(z = rep(1, constants$ncell),
+             alpha = runif(1,-1,1), 
+             b_forest = runif(1, -1, 1),
+             b_imperv = runif(1, -1, 1),
+             tau = rgamma(1, 1, 1), 
+             s = rep(0, base::length(data$num)),
+             p = runif(1, 0, 1)) }
+
 # parameters to monitor
 keepers <- c('psi', 'p', "alpha", 'b_forest', "b_imperv")
 
@@ -77,6 +91,11 @@ nb <- 1000 # number of initial MCMC iterations to discard
 ni <- nb + 1000 # total number  of iterations
 nt <- 5 # thinning interval
 
+# .......................................................................
+# PREPARE MODEL TO RUN
+# .......................................................................
+
+# create model
 model <- nimble::nimbleModel(code = joint, 
                              data = data, 
                              constants = constants, 
@@ -102,8 +121,34 @@ samples <- nimble::runMCMC(c_model_mcmc,
                            nchains = nc,
                            thin = nt)
 
-samples_mcmc <- as.mcmc(lapply(samples, mcmc))
+# .......................................................................
+# INSPECT RESULTS
+# .......................................................................
 
-coda::traceplot(samples_mcmc[, "b_forest"])
-coda::gelman.diag(samples_mcmc[,1:5])
-                  
+# convert to mcmc object for inspection via coda package
+samples_mcmc <- coda::as.mcmc(lapply(samples, coda::mcmc))
+
+# Look at traceplots of the first four parameters
+par(mfrow=c(2,2))
+coda::traceplot(samples_mcmc[, 1:4])
+# calculate Rhat convergence diagnostic for first four parameters
+coda::gelman.diag(samples_mcmc[,1:4])
+
+# extract mean and SD occurrence probability of each grid cell
+samples <- do.call(rbind, samples_mcmc)
+psi <- samples[, which(stringr::str_detect(string = colnames(samples), pattern = 'psi\\['))]
+psi_mean <- apply(psi, 2, mean)
+psi_sd <- apply(psi, 2, sd)
+
+# mean occurrence probability of the cells that contain testing data
+test_psi <- psi_mean[test$INDEX]
+
+# compute AUC to evaluate predictive performance
+pROC::roc(response = test$Y, predictor = test_psi, ci = TRUE)
+
+# map mean and standard deviation of occurrence probability
+par(mfrow=c(1,1))
+cov$psi_mean <- psi_mean
+plot(cov["psi_mean"])
+cov$psi_sd <- psi_sd
+plot(cov["psi_sd"])                  
